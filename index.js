466be18e30dc0fc2898ad98f5cdcdf9bdeb30403
monika-sampath <monikasampath.20@gmail.com>
2024-12-12 23:39:35 +0530

    monika-lms-bk

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 });
 
 // Server setup
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
